a90b242803418eaa90b0448f5207050c29f73dcb
Makisai <jschmaglowski@aol.com>
2025-05-19 19:33:56 +0200

    add Authentification

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -4,6 +4,6 @@ class Vendor < ApplicationRecord
     validates :name, presence: true
     validates :contact, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :phone, presence: true, format: { with: /\A\+?\d{10,15}\z/, message: "Bitte geben sie eine korrekte Telefonnummer ein" }
+    validates :phone, presence: true, format: { with: /\A\+?\d{10,15}\z/, message: I18n.t("validator.phone") }
     validates :address, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_212629) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_19_155633) do
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "stands", force: :cascade do |t|
     t.decimal "width"
     t.decimal "height"
@@ -30,6 +39,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_212629) do
     t.index ["vendor_id"], name: "index_stands_on_vendor_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   create_table "vendors", force: :cascade do |t|
     t.string "name"
     t.string "legalform"
@@ -45,5 +62,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_212629) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "sessions", "users"
   add_foreign_key "stands", "vendors"
 end
