7480bcf8dfaea95616c50194d1f8bf0fe4d13bfb
Makisai <jschmaglowski@aol.com>
2025-06-14 21:42:18 +0200

    add test model test for vendor

--- test/models/vendor_test.rb
+++ test/models/vendor_test.rb
@@ -22,4 +22,19 @@ class VendorTest < ActiveSupport::TestCase
 
     assert_equal "user@example.com", stand.vendor.last_changes_from
   end
+
+  test "#last_changes_from should return email of user with latest change to vendor or stand" do
+    user1 = FactoryBot.create(:user, email_address: "user1@example.com")
+    user2 = FactoryBot.create(:user, email_address: "user2@example.com")
+    
+    PaperTrail.request.whodunnit = user1.id.to_s
+    vendor = FactoryBot.create(:vendor)
+    stand = FactoryBot.create(:stand, vendor: vendor)
+    PaperTrail.request.whodunnit = user2.id.to_s
+    stand.update(description: "Updated description")
+
+    assert_equal "user1@example.com", vendor.last_changes_from
+    assert_equal "user2@example.com", stand.vendor.last_changes_from
+
+  end
 end
